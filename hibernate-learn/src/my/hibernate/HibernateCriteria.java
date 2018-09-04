f2e57e51cb4c9a37cdf6ea61a0cf7cfdc1218278
294355565@qq.com <294355565@qq.com>
2018-09-04 21:21:42 +0800

    jdbc hibernate mybatis learn

---
+++ hibernate-learn/src/my/hibernate/HibernateCriteria.java
@@ -0,0 +1,32 @@
+package my.hibernate;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+
+import my.hibernate.bean.Student;
+
+public class HibernateCriteria {
+
+	public static void main(String[] args) {
+		SessionFactory sf= new Configuration().configure().buildSessionFactory();
+		Session session=sf.openSession();
+		
+		System.out.println("-----------Criteria----------");
+		Criteria c = session.createCriteria(Student.class);
+		c.add(Restrictions.eq("id",new Integer(1024)));
+		
+		List<Student> students= c.list();
+		for(Student s:students) {
+			System.out.println(s);
+		}
+	session.close();
+		sf.close();
+
+	}
+
+}
